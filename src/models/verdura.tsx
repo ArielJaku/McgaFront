ba19d9fb5dbe24f703cdd1c99ff560a982736775
Cristian <cgalarzalppa@gmail.com>
2023-12-18 23:01:39 -0300

    agregando modelo, carpeta componente y limpiando app.ts

---
+++ src/models/verdura.tsx
@@ -0,0 +1,12 @@
+type verdura = {
+    id : string;
+    nombre : string;
+    tipo : string;
+    precio : number;
+    stock : number;
+    createAt : number;
+    updateAt : number;
+    _id:string
+}
+
+export default verdura
